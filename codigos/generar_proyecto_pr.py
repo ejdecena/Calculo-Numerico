480630db58cfa41f445fb723d950a2536ab72b5d
Edgard Decena <edecena@gmail.com>
2019-10-22 07:22:10 -0400

    Arreglo de función de cadenas.

--- codigos/generar_proyecto_pr.py
+++ codigos/generar_proyecto_pr.py
@@ -16,8 +16,8 @@ PREGUNTAS = [
 "Recibe como parámetro un diccionario y retorna el máximo valor del diccionario.",
 "Recibe como parámetro un diccionario y retorna el mínimo valor del diccionario.",
 "Recibe como parámetro a, b, c y retorma el máximo.",
-"Recibe como parámetro una cadena y retorna las vocales de la cadena.",
-"Recibe como parámetro una cadena y retorma las consonantes de la cadena.",
+"Recibe como parámetro una cadena y retorna una lista con las vocales de la cadena.",
+"Recibe como parámetro una cadena y retorma una lista con las consonantes de la cadena.",
 "Recibe como paŕametro una cadena y retorna el número de caracteres de la cadena.",
 "Recibe como parámetro una cadena y retorna la cadena invertida.",
 ]
